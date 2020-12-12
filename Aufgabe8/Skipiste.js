0ee9151af4b5c1da95b94a1a8a35fcd4ee75deca
amandaauer <48826856+amandaauer@users.noreply.github.com>
2020-12-12 18:00:27 +0100

    .

---
+++ Aufgabe8/Skipiste.js
@@ -0,0 +1,31 @@
+"use strict";
+var Skipiste;
+(function (Skipiste) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+    let golden = 0.62;
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2D");
+        drawBackground();
+        drawSun({ x: 100, y: 75 });
+        drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
+    }
+    function drawBackground() {
+        console.log("Background");
+        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(golden, "white");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawSun(_position) {
+        console.log("Sun", _position);
+    }
+    function drawCloud(_positon, _size) {
+        console.log("Cloud", _positon, _size);
+    }
+})(Skipiste || (Skipiste = {}));
+//# sourceMappingURL=Skipiste.js.map
