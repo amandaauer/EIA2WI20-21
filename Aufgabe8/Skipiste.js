10d0ba388567e114da1f704f8763228576851a66
amandaauer <48826856+amandaauer@users.noreply.github.com>
2020-12-19 18:51:25 +0100

    .

--- Aufgabe8/Skipiste.js
+++ Aufgabe8/Skipiste.js
@@ -1,6 +1,5 @@
-"use strict";
-var Skipiste;
-(function (Skipiste) {
+var L08_Canvas_Alley;
+(function (L08_Canvas_Alley) {
     window.addEventListener("load", handleLoad);
     let crc2;
     let golden = 0.62;
@@ -8,24 +7,100 @@ var Skipiste;
         let canvas = document.querySelector("canvas");
         if (!canvas)
             return;
-        crc2 = canvas.getContext("2D");
+        crc2 = canvas.getContext("2d");
+        let horizon = crc2.canvas.height * golden;
+        let posMountains = { x: 0, y: horizon };
         drawBackground();
         drawSun({ x: 100, y: 75 });
         drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
+        drawMountains(posMountains, 75, 200, "grey", "white");
+        drawMountains(posMountains, 50, 150, "grey", "lightgrey");
+        drawHill();
     }
     function drawBackground() {
         console.log("Background");
         let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
         gradient.addColorStop(0, "lightblue");
         gradient.addColorStop(golden, "white");
+        gradient.addColorStop(1, "HSL(100, 80%, 30%)");
         crc2.fillStyle = gradient;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
     function drawSun(_position) {
         console.log("Sun", _position);
+        let r1 = 30;
+        let r2 = 150;
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
+        gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
     }
-    function drawCloud(_positon, _size) {
-        console.log("Cloud", _positon, _size);
+    function drawCloud(_position, _size) {
+        console.log("Cloud", _position, _size);
+        let nParticles = 20;
+        let radiusParticle = 50;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
     }
-})(Skipiste || (Skipiste = {}));
+    function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
+        console.log("Mountains", _position, _min, _max);
+        let stepMin = 50;
+        let stepMax = 150;
+        let x = 0;
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -_max);
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y = -_min - Math.random() * (_max - _min);
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawHill() {
+        console.log("Piste");
+        crc2.beginPath();
+        crc2.moveTo(0, 550);
+        crc2.lineTo(crc2.canvas.width, crc2.canvas.height * golden + 50);
+        crc2.lineTo(crc2.canvas.width, crc2.canvas.height);
+        crc2.lineTo(0, crc2.canvas.height);
+        crc2.fillStyle = "white";
+        crc2.fill();
+        crc2.beginPath(); //Dreieck
+        crc2.moveTo(crc2.canvas.width, crc2.canvas.height * golden + 250);
+        crc2.lineTo(crc2.canvas.width, crc2.canvas.height);
+        crc2.lineTo(200, crc2.canvas.height);
+        crc2.fillStyle = "HSL(60, 50%, 30%";
+        crc2.fill();
+    }
+})(L08_Canvas_Alley || (L08_Canvas_Alley = {}));
 //# sourceMappingURL=Skipiste.js.map
